b12525527abc807181f77d88eecd307715b0cd6b
TAzmir <azmir.terzini@gmail.com>
2025-11-24 22:37:42 +0100

    added iso8601 date-only converter

---
+++ src/ProjX.Json/Converters/ISO8601DateOnlyConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ProjX.Json.Converters;
+
+public class ISO8601DateOnlyConverter: JsonConverter<DateOnly>
+{
+    private const string DATE_FORMAT = "yyyy-MM-dd";
+    private static readonly string _exampleDateOnly = DateOnly.FromDateTime(DateTime.UtcNow).ToString(DATE_FORMAT, CultureInfo.InvariantCulture);
+
+    public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var dateOnlyValue = reader.GetString();
+
+        if (DateOnly.TryParseExact(dateOnlyValue, DATE_FORMAT, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateOnly result))
+            return result;
+
+        throw new JsonException($"Invalid DateOnly value '{dateOnlyValue}'. Expected ISO 8601 Date-Only format: '{DATE_FORMAT}', e.g. '{_exampleDateOnly}'.");
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
+    {
+        var dateOnlyStr = value.ToString(DATE_FORMAT, CultureInfo.InvariantCulture);
+        writer.WriteStringValue(dateOnlyStr);
+    }
+}
---
+++ tests/ProjX.Json.Tests/Converters/ISO8601DateOnlyConverterTests.cs
@@ -0,0 +1,82 @@
+﻿using FluentAssertions;
+using ProjX.Json.Converters;
+using System.Globalization;
+using System.Text.Json;
+
+namespace ProjX.Json.Tests.Converters;
+
+public class ISO8601DateOnlyConverterTests
+{
+    private const string DATE_FORMAT = "yyyy-MM-dd";
+    private readonly JsonSerializerOptions _options;
+
+    public ISO8601DateOnlyConverterTests()
+    {
+        _options = new JsonSerializerOptions
+        {
+            Converters = { new ISO8601DateOnlyConverter() }
+        };
+    }
+
+    [Theory]
+    [InlineData("2025-11-24")]
+    public void WhenValidIso8601DateOnlyStr_ShouldSerialize(string dateOnlyStr)
+    {
+        // Arrange
+        var inputDate = DateOnly.ParseExact(dateOnlyStr, DATE_FORMAT, CultureInfo.InvariantCulture);
+        
+        // Act
+        var json = JsonSerializer.Serialize(inputDate, _options);
+
+        // Assert
+        json.Should().NotBeNullOrWhiteSpace();
+    }
+
+    [Theory]
+    [InlineData("2025-11-24")]
+    public void WhenValidIso8601DateOnlyStr_ShouldDeserialize(string dateOnlyStr)
+    {
+        // Arrange
+        var json = JsonSerializer.Serialize(dateOnlyStr, _options);
+
+        // Act
+        var deserializedDateOnly = JsonSerializer.Deserialize<DateOnly>(json, _options);
+
+        // Assert
+        deserializedDateOnly.Should().NotBe(DateOnly.MinValue);
+    }
+
+    [Theory]
+    [InlineData("2025-11-24-")]
+    [InlineData("11/24/2025-test")]
+    [InlineData("24/11/2025-00")]
+    [InlineData("")]
+    [InlineData(null)]
+    public void Deserialize_WhenInvalidFormats_ShouldThrowJsonException(string? inputDateString)
+    {
+        // Arrange
+        var json = JsonSerializer.Serialize(inputDateString, _options);
+
+        Action action = () =>
+        {
+            JsonSerializer.Deserialize<DateOnly>(json, _options);
+        };
+
+        //act
+        action.Should().Throw<JsonException>();
+    }
+
+    [Fact]
+    public void WhenSymmetrically_ShouldSerializeAndDeserialize()
+    {
+        // Arrange
+        var original = new DateOnly(2025, 11, 24);
+
+        // Act
+        var json = JsonSerializer.Serialize(original, _options);
+        var roundTrip = JsonSerializer.Deserialize<DateOnly>(json, _options);
+
+        // Assert
+        roundTrip.Should().Be(original);
+    }
+}
